bb32d315dcf96b1b6dc678284996568aed9b7977
Darío Ramos López <drl012@ual.es>
2021-07-22 12:04:13 +0200

    Some refactoring and small fixes in "simulateData"

--- R/rPACI.R
+++ R/rPACI.R
@@ -45,7 +45,6 @@ readCornealTopography <- function(filepath, ringsTotal = 24, pointsPerRing = 256
   
   length(numeric_lines)
   
-  
   dataPoints = pointsPerRing * ringsTotal
   
   
@@ -485,36 +484,42 @@ analyzeFolder <- function(path, fileExtension="txt", individualPlots = FALSE, su
 
 
 
-simulateData <- function(rings = 24, dataPerRing = 256, lastRingRadium = 8, ringRadiiNoise = 0, 
+simulateData <- function(rings = 24, pointsPerRing = 256, diameter = 12, ringRadiiPerturbation = 0, 
                          maximumMireDisplacement = 0, mireDisplacementAngle = 0, mireDisplacementNoise = 0,
                          ellipticAxesRatio = 1, ellipticRotation = 0, overallNoise = 0) {
   
-  dataPoints = dataPerRing * rings
+  dataPoints = pointsPerRing * rings
   
-  angles = seq(0,2*pi,length.out = 257)[1:256]
+  lastRingRadium = diameter/2
+  mireDisplacementAngleRad = pi/180*mireDisplacementAngle
+  ellipticRotationRad = pi/180*ellipticRotation  
+  
+  
+  angles = seq(0, 2*pi,length.out = (1+pointsPerRing))[1:pointsPerRing]
   angles = rep(angles, times = rings)
   
   radii = seq(0,lastRingRadium,length.out = rings+1)[2:(rings+1)]
-  radii = radii + ringRadiiNoise * rnorm(rings, sd = radii[1]/6)
-  radii = rep(radii, each = dataPerRing)
+  radii = radii + ringRadiiPerturbation * rnorm(rings, sd = radii[1]/6)
+  radii = sort(radii)
+  radii = rep(radii, each = pointsPerRing)
   
   # Adding a mire displacement in a certain direction (given by the angle )
   mireCentersRho = seq(0,maximumMireDisplacement,length.out = rings+1)[2:(rings+1)]
   mireCentersRho = mireCentersRho + mireDisplacementNoise * rnorm(rings, sd = mireCentersRho[1]/6)
   
-  mireCentersAngle = rep(mireDisplacementAngle, each = rings)
+  mireCentersAngle = rep(mireDisplacementAngleRad, each = rings)
   
   mireCentersX = mireCentersRho * cos(mireCentersAngle)
   mireCentersY = mireCentersRho * sin(mireCentersAngle)  
   
-  mireCentersX = rep(mireCentersX, each = dataPerRing)
-  mireCentersY = rep(mireCentersY, each = dataPerRing)
+  mireCentersX = rep(mireCentersX, each = pointsPerRing)
+  mireCentersY = rep(mireCentersY, each = pointsPerRing)
   
   
-  result=data.frame(matrix(NA, nrow = rings*dataPerRing, ncol = 0))
-  result["x"] = mireCentersX + radii * cos(ellipticRotation) * cos(angles) - radii * ellipticAxesRatio * sin(ellipticRotation) * sin(angles) + overallNoise * rnorm(dataPoints,sd=0.1)
-  result["y"] = mireCentersY + radii * sin(ellipticRotation) * cos(angles) + radii * ellipticAxesRatio * cos(ellipticRotation) * sin(angles) + overallNoise * rnorm(dataPoints,sd=0.1)
-  result["ring index"] = kronecker(1:rings,rep(1,dataPerRing))
+  result=data.frame(matrix(NA, nrow = rings*pointsPerRing, ncol = 0))
+  result["x"] = mireCentersX + radii * cos(ellipticRotationRad) * cos(angles) - radii * ellipticAxesRatio * sin(ellipticRotationRad) * sin(angles) + overallNoise * rnorm(dataPoints,sd=0.1)
+  result["y"] = mireCentersY + radii * sin(ellipticRotationRad) * cos(angles) + radii * ellipticAxesRatio * cos(ellipticRotationRad) * sin(angles) + overallNoise * rnorm(dataPoints,sd=0.1)
+  result["ring index"] = kronecker(1:rings,rep(1,pointsPerRing))
   
   
   colnames(result) = c("x","y","ring index")
